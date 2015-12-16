99a53434a64e982f5ebf907bbe268d56042fab3d
Mayur Ahir <mayur@playgen.com>
2015-12-16 12:34:38 +0000

    Models
    - Account
    
    - Session (Partial)

---
+++ SocialGamificationAsset/Models/Account.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialGamificationAsset.Models
+{
+	public class Account
+	{
+		public Guid Id { get; set; }
+
+		public string Username { get; set; }
+
+		public string Password { get; set; }
+
+		public string Email { get; set; }
+
+		public Guid ActivationCode { get; private set; }
+
+		public bool IsEnabled { get; set; }
+
+		public AccountTypeEnum Type { get; set; }
+
+		public string LastLoginIp { get; set; }
+
+		public DateTime CreatedDate { get; set; }
+
+		public DateTime LastLoginDate { get; set; }
+
+		public virtual ICollection<Session> Sessions { get; set; }
+
+		/**
+		 * Check if the current Username already exists
+         *
+         * @return boolean Returns TRUE if Username exists
+         */
+
+		public bool ExistsUsername()
+		{
+			return false;
+		}
+
+		/**
+		 * Check if the current Email already exists
+		 *
+		 * @return boolean Returns TRUE if Username exists
+		 */
+
+		public bool ExistsEmail()
+		{
+			return false;
+		}
+
+		/**
+		 * Verify if this account is logged
+		 *
+		 * @return bool Returns TRUE if the account is logged
+		 */
+
+		public bool IsLogged()
+		{
+			return false;
+		}
+
+		/**
+		 * Verify if this account is online (with Last session being marked active)
+		 *
+		 * @return bool Returns TRUE if the account is online
+		 */
+
+		public bool IsOnline()
+		{
+			return false;
+		}
+
+		/**
+		 * Get the last session
+		 *
+		 * @return Session
+		 */
+
+		public Session GetLastActionDate()
+		{
+			return new Session();
+		}
+
+		/**
+		 * Get the current session of Logged account
+		 *
+		 * @return Session
+		 */
+
+		public Session GetSession()
+		{
+			return new Session();
+		}
+
+		/**
+		 * Get the registered accounts
+		 *
+		 * @return IList<Account> the array of accounts
+		 */
+
+		public IList<Account> Load()
+		{
+			IList<Account> accounts = new List<Account>();
+			return accounts;
+		}
+	}
+
+	public enum AccountTypeEnum
+	{
+		Player = 0,
+		Admin = 1,
+	}
+}
---
+++ SocialGamificationAsset/Models/Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialGamificationAsset.Models
+{
+	public class Session
+	{
+		public Guid Id { get; set; }
+
+		public Guid AccountId { get; set; }
+
+		[ForeignKey("AccountId")]
+		public virtual Account Account { get; set; }
+	}
+}
