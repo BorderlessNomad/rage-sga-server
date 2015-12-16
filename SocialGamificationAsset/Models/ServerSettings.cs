525402745900c145646baf2a3542c6fb7714f71e
Jack Cannon <jack@playgen.com>
2015-12-16 12:08:21 +0000

    Add models S+

---
+++ SocialGamificationAsset/Models/ServerSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialGamificationAsset.Models
+{
+    public class ServerSettings
+    {
+        public string DataKey { get; set; }
+
+        public string DataValue { get; set; }
+    }
+}
---
+++ SocialGamificationAsset/Models/Session.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialGamificationAsset.Models
+{
+    public class Session
+    {
+        public Guid Id { get; set; }
+
+        public Guid IdAccount { get; set; }
+
+        public Guid GUID { get; set; }
+
+        public DateTime LastActionDate { get; set; }
+
+        public string LastActionIP{ get; set; }
+
+        public DateTime SignitureTimestamp { get; set; }
+    }
+}
