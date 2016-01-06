6502700cc0954692b6687eee4618a396ed4c62b9
Matthieu Lepinay <matt@playgen.com>
2016-01-06 11:24:04 +0000

    Basic group creations with list of actors

--- SocialGamificationAsset/Models/Group.cs
+++ SocialGamificationAsset/Models/Group.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace SocialGamificationAsset.Models
 {
@@ -15,13 +16,17 @@ namespace SocialGamificationAsset.Models
 		/// <summary>
 		/// Create the group for the first time. Needs to populate the fake actor and get the list of player actors.
 		/// </summary>
-		public void AddActors(ICollection<Actor> actors)
+		public void AddActors(SocialGamificationAssetContext db, ICollection<Actor> actorsList)
 		{
-			if (actors.Count < 2)
+			if (actorsList.Count < 2)
 			{
 				Console.WriteLine("At least two users are needed to create a group");
 				return;
 			}
+
+			List<Guid> actorIds = actorsList.Select(a => a.Id).ToList();
+
+			List<Actor> actors = db.Actors.Where(a => actorIds.Contains(a.Id)).ToList();
 
 			this.Actors = new List<Actor>(actors);
 		}
