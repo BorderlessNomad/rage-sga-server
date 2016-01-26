58cdfb14de178e21346575816a691277d155c8d5
Mayur Ahir <mayur@playgen.com>
2016-01-26 17:26:47 +0000

    Housekeeping.

--- SocialGamificationAsset/Models/Role.cs
+++ SocialGamificationAsset/Models/Role.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SocialGamificationAsset.Models
+﻿namespace SocialGamificationAsset.Models
 {
 	public class Role : Model
 	{
