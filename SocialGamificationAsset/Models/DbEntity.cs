3c36de212f45de9860145e5a8f704bed07171f50
Mayur Ahir <mayur@playgen.com>
2016-02-17 15:52:25 +0000

    Start Unity Test.

--- SocialGamificationAsset/Models/DbEntity.cs
+++ SocialGamificationAsset/Models/DbEntity.cs
@@ -6,15 +6,24 @@ using Newtonsoft.Json;
 
 namespace SocialGamificationAsset.Models
 {
+    /// <summary>
+    /// DBEntity Interface
+    /// </summary>
     public interface IDbEntity
     {
         Guid Id { get; set; }
     }
 
+    /// <summary>
+    /// DbEntity class handling Primary Key creation & other default parameters.
+    /// </summary>
     [JsonObject(IsReference = true)]
     [DataContract]
     public class DbEntity : IDbEntity
     {
+        /// <summary>
+        /// Default constructor initialising <see cref="Id"/>.
+        /// </summary>
         public DbEntity()
         {
             Id = Guid.NewGuid();
@@ -27,10 +36,19 @@ namespace SocialGamificationAsset.Models
 		public virtual GameRegistry Game { get; set; }
 		*/
 
+        /// <summary>
+        /// Set default Updated Date field.
+        /// </summary>
         public DateTime? UpdatedDate { get; set; }
 
+        /// <summary>
+        /// Set defauly Created Date field.
+        /// </summary>
         public DateTime CreatedDate { get; set; }
 
+        /// <summary>
+        /// Custom Primary Key set to <see cref="Guid"/>.
+        /// </summary>
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid Id { get; set; }
     }
