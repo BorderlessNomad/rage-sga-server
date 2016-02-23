0deea59135befea71924bcf3613bce78626a8f37
Mayur Ahir <mayur@playgen.com>
2016-02-23 17:30:46 +0000

    Session API Integration Tests.

--- SocialGamificationAsset/Tests/Controllers/SessionsControllerTest.cs
+++ SocialGamificationAsset/Tests/Controllers/SessionsControllerTest.cs
@@ -12,6 +12,86 @@ namespace SocialGamificationAsset.Tests.Controllers
     public class SessionsControllerTest : ControllerTest
     {
         [Fact]
+        public async Task LoginWithEmptyForm()
+        {
+            using (var client = _server.AcceptJson())
+            {
+                // login with Empty UserForm
+                var loginFormEmpty = new UserForm { };
+
+                var loginResponse = await client.PostAsJsonAsync("/api/sessions", loginFormEmpty);
+                Assert.Equal(HttpStatusCode.BadRequest, loginResponse.StatusCode);
+
+                var created = await loginResponse.Content.ReadAsJsonAsync<ApiError>();
+                Assert.Equal("Either Username or Email is required.", created.Error);
+            }
+        }
+
+        [Fact]
+        public async Task LoginWithNoUser()
+        {
+            using (var client = _server.AcceptJson())
+            {
+                // login with No Username/Email
+                var loginFormNoUsernameOrEmail = new UserForm { Password = "mayur" };
+
+                var loginResponse = await client.PostAsJsonAsync("/api/sessions", loginFormNoUsernameOrEmail);
+                Assert.Equal(HttpStatusCode.BadRequest, loginResponse.StatusCode);
+
+                var created = await loginResponse.Content.ReadAsJsonAsync<ApiError>();
+                Assert.Equal("Either Username or Email is required.", created.Error);
+            }
+        }
+
+        [Fact]
+        public async Task LoginWithNoPassword()
+        {
+            using (var client = _server.AcceptJson())
+            {
+                // login with No Password
+                var loginWithNoPassword = new UserForm { Username = "mayur" };
+
+                var loginResponse = await client.PostAsJsonAsync("/api/sessions", loginWithNoPassword);
+                Assert.Equal(HttpStatusCode.BadRequest, loginResponse.StatusCode);
+
+                var created = await loginResponse.Content.ReadAsJsonAsync<ApiError>();
+                Assert.Equal("Password is required.", created.Error);
+            }
+        }
+
+        [Fact]
+        public async Task LoginWithUnknonUser()
+        {
+            using (var client = _server.AcceptJson())
+            {
+                // login with Unknown User
+                var loginFormUnkownUser = new UserForm { Username = "unknown", Password = "unknown" };
+
+                var loginResponse = await client.PostAsJsonAsync("/api/sessions", loginFormUnkownUser);
+                Assert.Equal(HttpStatusCode.NotFound, loginResponse.StatusCode);
+
+                var created = await loginResponse.Content.ReadAsJsonAsync<ApiError>();
+                Assert.Equal("No such Player found.", created.Error);
+            }
+        }
+
+        [Fact]
+        public async Task LoginWithInvalidPassword()
+        {
+            using (var client = _server.AcceptJson())
+            {
+                // login with Invalid Password
+                var loginFormInvalidPassword = new UserForm { Username = "mayur", Password = "unknown" };
+
+                var loginResponse = await client.PostAsJsonAsync("/api/sessions", loginFormInvalidPassword);
+                Assert.Equal(HttpStatusCode.Unauthorized, loginResponse.StatusCode);
+
+                var created = await loginResponse.Content.ReadAsJsonAsync<ApiError>();
+                Assert.Equal("Invalid Login Details.", created.Error);
+            }
+        }
+
+        [Fact]
         public async Task LoginThenObtainSession()
         {
             using (var client = _server.AcceptJson())
@@ -70,7 +150,7 @@ namespace SocialGamificationAsset.Tests.Controllers
 
                 // get session
                 var sessionResponse = await client.GetAsync($"/api/sessions/{sessionId}");
-                Assert.Equal(sessionResponse.StatusCode, HttpStatusCode.NotFound);
+                Assert.Equal(HttpStatusCode.NotFound, sessionResponse.StatusCode);
             }
         }
 
@@ -83,7 +163,7 @@ namespace SocialGamificationAsset.Tests.Controllers
 
                 // get session
                 var sessionResponse = await client.GetAsync($"/api/sessions/{sessionId}");
-                Assert.Equal(sessionResponse.StatusCode, HttpStatusCode.NotFound);
+                Assert.Equal(HttpStatusCode.NotFound, sessionResponse.StatusCode);
 
                 var fetched = await sessionResponse.Content.ReadAsJsonAsync<ApiError>();
                 Assert.Equal("No such Session found.", fetched.Error);
