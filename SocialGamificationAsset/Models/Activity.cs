6d2315b308050bbfb99e9021715b1cd0523d8ae4
Matthieu Lepinay <matt@playgen.com>
2016-03-29 10:25:49 +0100

    Admin panel for Users, Matches, Activities

--- SocialGamificationAsset/Models/Activity.cs
+++ SocialGamificationAsset/Models/Activity.cs
@@ -3,14 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SocialGamificationAsset.Models
 {
-    public class Activity : DbEntity
-    {
-        [Required]
-        public string Name { get; set; }
+	public class Activity : DbEntity
+	{
+		[Required]
+		public string Name { get; set; }
 
-        public string Description { get; set; }
+		public string Description { get; set; }
 
-        public string Image { get; set; }
+		public string Image { get; set; }
 
         // [IgnoreDataMember]
         public virtual ICollection<AttributeType> Skills { get; set; }
@@ -26,4 +26,11 @@ namespace SocialGamificationAsset.Models
     {
         public string Description { get; set; }
     }
+
+	public class ActivityDropdown
+	{
+		public string Id { get; set; }
+
+		public string Name { get; set; }
+	}
 }
